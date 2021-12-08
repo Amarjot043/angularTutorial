17da6ac12fd819e028e5ca228db75792245ef78f
Amarjot <amarjot043@gmail.com>
2021-12-08 09:59:20 +0530

    angular new commit

---
+++ src/app/shared/convert-to-spaces.pipe.ts
@@ -0,0 +1,8 @@
+import {PipeTransform} from "@angular/core";
+
+export class ConvertToSpacesPipe implements PipeTransform {
+    transform(value: string, ...args: any[]) {
+        throw new Error("Method not implement ");
+    }
+
+}
